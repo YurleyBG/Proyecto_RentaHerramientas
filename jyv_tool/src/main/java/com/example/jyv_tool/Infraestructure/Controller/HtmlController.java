9813590893e1f83c8402ac602ace172e83b5e91c
YurleyBG <yurleybgarcia20@gmail.com>
2025-05-18 00:28:00 -0500

    feat :sparkles: connection html,css,javascript

---
+++ jyv_tool/src/main/java/com/example/jyv_tool/Infraestructure/Controller/HtmlController.java
@@ -0,0 +1,14 @@
+package com.example.jyv_tool.Infraestructure.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.ui.Model;
+
+@Controller
+public class HtmlController {
+    @GetMapping("/Home")
+    public String index(Model model) {
+        return "PaginaInicio"; 
+    }
+}
